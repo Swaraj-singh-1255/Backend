242fce9f35c579ca9468a3d5a29de1d106e85a9d
Swaraj-singh-1255 <swarajsinghrajput1255@gmail.com>
2026-02-22 11:36:20 +0530

    Rename login.jsx to Login.jsx

---
+++ Day-14/Frontend/src/app.routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router"
+import Login from "./features/auth/pages/Login"
+
+
+const router = createBrowserRouter([
+    {
+        path: "/login"
+    }
+])
