09ce5be6407ac40184ecefc69a2e5eea8da37c27
Swaraj-singh-1255 <swarajsinghrajput1255@gmail.com>
2026-03-24 02:06:05 +0530

    feat: integrate Socket.IO for real-time communication and update authentication flow

--- Day-17-Perplexity/Frontend/src/features/auth/hook/userAuth.js
+++ Day-17-Perplexity/Frontend/src/features/auth/hook/userAuth.js
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { register, login, getMe } from "../service/auth.api";
+import { register, login, getMe } from "../../chat/service/auth.api";
 import { setUser, setLoading, setError } from "../auth.slice";
 
 
